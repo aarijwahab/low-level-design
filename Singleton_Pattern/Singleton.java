687890e014ede5d8b6159717f48355c407b4e3e5
Aarij <aarijwahab@gmail.com>
2025-04-07 18:41:44 -0400

    added singleton

---
+++ Singleton_Pattern/Singleton.java
@@ -0,0 +1,15 @@
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton(){}
+
+
+    // For race conditions this will need to be synchronized
+    public static Singleton getInstance(){
+        if (uniqueInstance == null){
+            uniqueInstance = new Singleton();
+        }
+
+        return uniqueInstance;
+    }
+}
