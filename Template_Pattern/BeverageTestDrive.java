9defe497baef4b5eb38084a4d5f4818ebfb34c00
Aarij Wahab <aarijwahab@gmail.com>
2025-04-08 21:52:59 -0400

    added template pattern

---
+++ Template_Pattern/BeverageTestDrive.java
@@ -0,0 +1,13 @@
+package Template_Pattern;
+
+
+public class BeverageTestDrive {
+    public static void main(String[] args) {
+        Tea teaHook = new Tea();
+        Coffee coffeeHook = new Coffee();
+        System.out.println("\nMaking tea...");
+        teaHook.prepareRecipe();
+        System.out.println("\nMaking coffee...");
+        coffeeHook.prepareRecipe();
+    }
+}
